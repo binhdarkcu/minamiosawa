1463b305b404c514922f735b2a85767fec3aab0a
Binh Lam <binh@bliss-interactive.net>
2016-04-03 23:53:27 +0700

    update plan page desktop

--- leafia_minamiosawa_4th/plans/js/plan.js
+++ leafia_minamiosawa_4th/plans/js/plan.js
@@ -2,23 +2,32 @@
 var planPage = (function() {
 	function init(){
 		outRoom();
+		closePopup();
 	}
 	
 	function openRoom(idDiv){
-		
+		$('.popup').css('display', 'none');
+		$('#' + idDiv).css('display', 'block');
 	}
 	function hoverRoom(idDiv){
-		$('.room .r').css('display', 'none');
+		$('.room .r, .selectRoom .map').css('display', 'none');
 		$('#' + idDiv).css('display', 'block');
 	}
 	function outRoom(){
 		$('.selectRoom a.choose').mouseout(function(){
 			$('.room .r').css('display', 'none');
+			$('.selectRoom .map').css('display', 'block');
+		});
+	}
+	function closePopup(){
+		$('.close').click(function(){
+			$('.popup').css('display','none');
 		});
 	}
 	return {
 		init:init,
-		hoverRoom:hoverRoom
+		hoverRoom:hoverRoom,
+		openRoom:openRoom
 	}
 	
 })();
