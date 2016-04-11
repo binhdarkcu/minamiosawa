ae8f88416a06482ad2fad71a425ff3b75c97d478
tungken <whynot020689@gmail.com>
2016-04-11 22:09:26 +0700

    fix bug

--- leafia_minamiosawa_4th/equipment/js/tab.js
+++ leafia_minamiosawa_4th/equipment/js/tab.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 	$('ul.tabs li').click(function(e){
         e.preventDefault();
 		var tab_id = $(this).attr('data-tab');
-
+        
 		$('ul.tabs li').removeClass('current');
+		$("li[data-tab='"+tab_id+"']").addClass('current');
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
--- leafia_minamiosawa_4th/leafia/js/tab.js
+++ leafia_minamiosawa_4th/leafia/js/tab.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		var tab_id = $(this).attr('data-tab');
 
 		$('ul.tabs li').removeClass('current');
+		$("li[data-tab='"+tab_id+"']").addClass('current');
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
